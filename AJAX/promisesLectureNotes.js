bc551865f257d7d3ec89310773a48fed959af450
jongodinez <jonathanjgodinez@gmail.com>
2018-11-28 15:32:09 -0800

    Update promisesLectureNotes.js

--- AJAX/promisesLectureNotes.js
+++ AJAX/promisesLectureNotes.js
@@ -73,3 +73,9 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(resp=> resp.json())
     .then(console.log)
 
+async function fetchUsers() {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await resp.json()
+    console.log(data);
+}
+
