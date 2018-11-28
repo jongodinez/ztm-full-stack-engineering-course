896f016eaafb0393b071cc3095856d2c4b004392
jongodinez <jonathanjgodinez@gmail.com>
2018-11-28 15:28:19 -0800

    Update promisesLectureNotes.js

--- AJAX/promisesLectureNotes.js
+++ AJAX/promisesLectureNotes.js
@@ -54,3 +54,17 @@ const successPromise = new Promise((resolve, reject) => {
 
 Promise.all([successPromise])
     .then(console.log('success'));
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+movePlayer(100, 'Left')
+    .then(() => movePlayer(400, 'Left'))
+    .then(() => movePlayer(10, 'Right'))
+    .then(() => movePlayer(330, 'Left'))
+
+async function playerStart() {
+    await movePlayer(100, 'Left')
+    await movePlayer(400, 'Left')
+    await movePlayer(10, 'Right')
+    await movePlayer(330, 'Left')
+}
